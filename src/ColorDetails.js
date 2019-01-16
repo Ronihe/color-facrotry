a45273de52ffafbede7ccd43932a1f5bda861f2d
P Virk <virk@outlook.com>
2019-01-15 17:49:54 -0800

    minor change in comments

--- src/ColorDetails.js
+++ src/ColorDetails.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 //this.props.match.params.color
 class ColorDetails extends Component {
   render() {
+    // we need to make sure the color is exsitng in parens't state
     return <div>{this.props.match.params.color}</div>;
   }
 }
